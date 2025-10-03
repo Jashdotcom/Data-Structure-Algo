286a21e998e34877c9ceb3e48d602bbb9877b1ab
Jash N. Mistry <jashmistry23@gmail.com>
2025-10-03 19:56:54 +0530

    Add files via upload

---
+++ SecondLargest.java
@@ -0,0 +1,54 @@
+package DSA;
+
+import java.util.Scanner;
+
+public class SecondLargest {
+
+    public static void main(String[] args){
+
+        //SECOND LARGEST VAR IS STORED AS -1 AS WE ARE ASSUMING ARRAY CONTAINS ALL POSITIVE ELEMENTS
+
+        Scanner sc = new Scanner(System.in);
+
+        int size=0;
+        System.out.print("Enter the size of array : ");
+        size = sc.nextInt();
+        sc.nextLine();
+
+        int[] arr = new int[size];
+
+        System.out.println();
+        System.out.println("Enter the array elements : ");
+        for(int i=0;i<arr.length;i++){
+            System.out.printf("Element %d : ",i);
+            arr[i]=sc.nextInt();
+            sc.nextLine();
+        }
+
+        int largest=arr[0];
+        int secLargest=-1;
+        int i=0;
+
+        for(i=1;i<arr.length;i++){
+            if(arr[i]>largest){
+                largest=arr[i];
+                secLargest=arr[i-1];
+            }
+            else if(arr[i]<largest && arr[i]>secLargest){
+                secLargest=arr[i];
+            }
+        }
+        System.out.println();
+        System.out.print("Array is : ");
+        for(i=0;i<arr.length;i++){
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+        System.out.print("Second Largest : " + secLargest);
+
+
+
+
+
+    }
+}
