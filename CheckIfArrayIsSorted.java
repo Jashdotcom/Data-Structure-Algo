15d357ea4ac1e9f98f61fb330933036e28af1ae4
Jash N. Mistry <jashmistry23@gmail.com>
2025-10-03 21:01:00 +0530

    Add files via upload

---
+++ CheckIfArrayIsSorted.java
@@ -0,0 +1,46 @@
+package DSA;
+
+import java.util.Scanner;
+
+public class CheckIfArrayIsSorted {
+
+    // THIS CODE IS FOR NON-DESCENDING ARRAY
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the size of array : ");
+        int size = sc.nextInt();
+
+        int[] arr = new int[size];
+
+        System.out.println("\nEnter the array elements : ");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.printf("Element %d : ", i);
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.print("\nArray : ");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
+        boolean sorted = true;
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            if (arr[i] <= arr[i + 1]) {
+            }
+            else {
+                sorted = false;
+                break;
+            }
+        }
+        if (sorted){
+            System.out.println("\nThe Array is sorted");
+        }
+        else{
+            System.out.println("\nThe Array is not sorted");
+        }
+    }
+}
