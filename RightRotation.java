a87e964cbe9476e3560270f507961c57025e00dd
Jash N. Mistry <jashmistry23@gmail.com>
2025-10-11 16:15:45 +0530

    Add files via upload

---
+++ RightRotation.java
@@ -0,0 +1,57 @@
+package DSA;
+
+import java.util.Scanner;
+public class RightRotation {
+
+    public static void main(String[] args) {
+
+        //RIGHT ROTATING AN ARRAY BY N PLACES
+        //OPTIMAL SOLUTION
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the size of array : ");
+        int size = sc.nextInt();
+
+        int[] arr = new int[size];
+
+        System.out.println("\nEnter the array elements : ");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.printf("Element %d : ", i);
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.print("\nEnter number of place/s by which the array should be rotated : ");
+        int num = sc.nextInt();
+
+        System.out.print("\nArray before rotations : ");
+        for(int i : arr){
+            System.out.print(i + " ");
+        }
+
+        int orgNum=num;
+        num = num % size;
+
+
+        reverse(arr, 0, size-1);
+        reverse(arr, 0, num - 1);
+        reverse(arr, num, size-1);
+
+
+        System.out.print("\nArray after " + orgNum + " right rotation/s : ");
+        for (int j : arr) {
+            System.out.print(j + " ");
+        }
+
+        sc.close();
+    }
+    public static void reverse(int[] arr, int start, int end) {
+        while (start < end) {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+    }
+}
